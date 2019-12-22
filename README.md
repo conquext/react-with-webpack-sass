b85bf705e6fa68684d44630c162f8c810aa95aff
Farce <intbusfor@gmail.com>
2019-12-22 17:44:13 +0100

    Optimize build for production

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Cd into project directory,
 App runs in the development mode.<br>
 Open [http://localhost:8080](http://localhost:8080) to view it in the browser.
 
-### `npm run build`
+### `npm run build:prod`
 
 Builds the app for production to the `build` folder.<br>
 It bundles React in production mode and optimizes the build for the best performance.
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,36 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: "./src/app.js",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
-      {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "public/index.html"
-    })
-  ],
-  devtool: "cheap-module-eval-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "public")
-  }
+module.exports = env => {
+  const isProduction = env === "production";
+  return {
+    entry: "./src/app.js",
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: ["style-loader", "css-loader", "sass-loader"]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "public/index.html"
+      })
+    ],
+    devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true
+    }
+  };
 };
